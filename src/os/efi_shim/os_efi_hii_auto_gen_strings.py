084360a601809c0ceebc01a9eb213dddffcf3867
Darin K Hoff <darin.k.hoff@intel.com>
2018-08-10 01:50:04 +0200

    Rename UEFI package
    Signed-off-by: Darin K Hoff <darin.k.hoff@intel.com>
    Signed-off-by: Juston Li <juston.li@intel.com>

--- src/os/efi_shim/os_efi_hii_auto_gen_strings.py
+++ src/os/efi_shim/os_efi_hii_auto_gen_strings.py
@@ -61,8 +61,8 @@ if __name__ == '__main__':
 	uefiRootPath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 	# Later files override earlier files
 	inputFiles = [
-	os.path.join(uefiRootPath,'DcpmemPkg','common','NvmStatus.uni'),
-	os.path.join(uefiRootPath,'DcpmemPkg','driver','Core','Diagnostics','DiagnosticsMessages.uni')]
+	os.path.join(uefiRootPath,'DcpmPkg','common','NvmStatus.uni'),
+	os.path.join(uefiRootPath,'DcpmPkg','driver','Core','Diagnostics','DiagnosticsMessages.uni')]
 	outputFileDir = os.path.dirname(os.path.realpath(__file__))
 	# Output to the same directory
 	# Different headers because everyone needs indices (defs) but not the actual strings

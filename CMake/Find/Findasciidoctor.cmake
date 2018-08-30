e99ee9671f1a539f93ab2bf8abd416b7ebae05fe
Juston Li <juston.li@intel.com>
2018-08-30 22:33:16 +0200

    documentation: clean up manpages
    * Fix a file name typo with ipmctl-show-system-capabilites.txt
    * Fix manpages to use os_build attribute
    * align PROPERTIES descriptions
    
    Signed-off-by: Juston Li <juston.li@intel.com>

--- CMake/Find/Findasciidoctor.cmake
+++ CMake/Find/Findasciidoctor.cmake
@@ -3,12 +3,13 @@
 
 # Try to find asciidoctor 
 # Once done this will define
-# ASCIIDOCTOR_FOUND - numactl found
-# ASCIIDOCTOR_INCLUDE_DIRS - numactl include directories
-# ASCIIDOCTOR_LIBRARIES - libraries needed to use numactl
+# ASCIIDOCTOR_FOUND - asciidoctor found
 
 find_program(ASCIIDOCTOR_BINARY NAMES asciidoctor
 	HINTS ${ASCIIDOCTOR_BINARY_PATH})
+
+find_program(ASCIIDOCTOR_PDF_BINARY NAMES asciidoctor-pdf
+	HINTS ${ASCIIDOCTOR_PDF_BINARY_PATH})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set ASCIIDOCTOR_FOUND to TRUE
@@ -16,10 +17,18 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(asciidoctor DEFAULT_MSG
                                   ASCIIDOCTOR_BINARY)
 
+find_package_handle_standard_args(asciidoctor-pdf DEFAULT_MSG
+                                  ASCIIDOCTOR_PDF_BINARY)
+
 mark_as_advanced(ASCIIDOCTOR_BINARY)
 
 set(ASCIIDOCTOR_BINARIES ${ASCIIDOCTOR_BINARY})
+set(ASCIIDOCTOR_PDF_BINARIES ${ASCIIDOCTOR_PDF_BINARY})
 
 if(NOT ASCIIDOCTOR_FOUND)
 	MESSAGE("No documentation will be generated.")
+endif()
+
+if(NOT ASCIIDOCTOR-PDF_FOUND)
+	MESSAGE("asciidoctor-pdf not found")
 endif()

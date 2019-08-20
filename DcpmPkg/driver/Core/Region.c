2a19a0d45367e37b54a75135d8e83c7b31560085
James Anandraj <james.sushanth.anandraj@intel.com>
2019-08-20 22:26:06 +0200

    Ignore any unmapped (non-POR) DCPMMs, allowing goal creation for the remaining DCPMMs in POR config.
    Signed-off-by: James Sushanth Anandraj <james.sushanth.anandraj@intel.com>

--- DcpmPkg/driver/Core/Region.c
+++ DcpmPkg/driver/Core/Region.c
@@ -1744,7 +1744,7 @@ VerifyCreatingSupportedRegionConfigs(
       pDimm = DIMM_FROM_NODE(pDimmNode);
 
       if (Socket == pDimm->SocketId) {
-        if (!IsDimmManageable(pDimm)) {
+        if (!IsDimmManageable(pDimm) || !IsDimmInSupportedConfig(pDimm)) {
           continue;
         }
 
@@ -1762,7 +1762,7 @@ VerifyCreatingSupportedRegionConfigs(
     /** Get a number of specified configured and unconfigured DIMMs on a given socket **/
     for (Index = 0; Index < DimmsNum; Index++) {
       if (Socket == pDimms[Index]->SocketId) {
-        if (!IsDimmManageable(pDimms[Index])) {
+        if (!IsDimmManageable(pDimms[Index]) || !IsDimmInSupportedConfig(pDimms[Index])) {
           continue;
         }
 

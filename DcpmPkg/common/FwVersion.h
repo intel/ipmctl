4176f03a5050b3c53e933b612dad222f3135a978
Steven Pontsler <steven.pontsler@intel.com>
2020-01-10 16:29:17 -0800

    Add file FwVersion.h

---
+++ DcpmPkg/common/FwVersion.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018, Intel Corporation.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _FWVERSION_H_
+#define _FWVERSION_H_
+
+/** Minimum supported version of FW API: 1.2 **/
+#define MIN_FIS_SUPPORTED_BY_THIS_SW_MAJOR   1
+#define MIN_FIS_SUPPORTED_BY_THIS_SW_MINOR   2
+/** Maximum supported version of FW API: 2.3 **/
+#define MAX_FIS_SUPPORTED_BY_THIS_SW_MAJOR    2
+#define MAX_FIS_SUPPORTED_BY_THIS_SW_MINOR    3
+
+#endif /** _FWVERSION_H_ **/

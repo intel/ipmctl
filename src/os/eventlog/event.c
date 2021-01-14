a8f4b77c003ede34f9200b624e99fa3dac9c5f29
Chris Ye <chris.ye@intel.com>
2021-01-14 15:14:03 +0800

    Replace the environment variable of %APPDATA%
    TEMP_FILE_PATH configured as %APPDATA% in ipmctl_default.ini did not replaced with the environment
    variable, app will create %APPDATA% directly under current cmd execution path, this is not good behavior.
    The %APPDATA% should be replaced as real value such as to "C:\Users\Administrator\AppData\Roaming".
    
    Fix the issue: https://github.com/intel/ipmctl/issues/157
    
    Signed-off-by: Chris Ye <chris.ye@intel.com>

--- src/os/eventlog/event.c
+++ src/os/eventlog/event.c
@@ -141,6 +141,26 @@ static EFI_STATUS get_the_system_log_file_name(log_file_type file, UINTN file_si
     if (FALSE == g_log_file_table[SYSTEM_LOG_AR_FILE].name_initialized) {
       efi_status = preferences_get_string_ascii(g_log_file_table[SYSTEM_LOG_AR_FILE].ini_entry_name, guid, SYSTEM_LOG_FILE_NAME_MAX_LEN, g_log_file_table[SYSTEM_LOG_AR_FILE].file_name);
       if (EFI_SUCCESS == efi_status) {
+        // Overwrite environment variables
+        // Find the environment variable control chars
+        p_env_start = strchr(g_log_file_table[SYSTEM_LOG_AR_FILE].file_name, ENVIRONMENT_VARIABLE_CHAR_START);
+        if (NULL != p_env_start)
+        {
+          p_env_start++; // start + 1 cause we have to skip the ENVIRONMENT_VARIABLE_CHAR_START char
+          p_env_stop = strchr(p_env_start, ENVIRONMENT_VARIABLE_CHAR_STOP);
+          if (NULL != p_env_stop) {
+            // Replace the environment variable with the real value
+            environment_variable[0] = 0; // Initialize the environemt variable string as empty
+            strncat_s(environment_variable, ENVIRONMENT_VARIABLE_MAX_LEN, p_env_start, p_env_stop - p_env_start);
+            if (NULL != (p_env_variable_path = getenv(environment_variable))) {
+              snprintf(temp_file_name, SYSTEM_LOG_FILE_NAME_MAX_LEN, "%s", p_env_variable_path);
+            }
+            p_env_stop++; // stop + 1 cause we have to skip the ENVIRONMENT_VARIABLE_CHAR_STOP char
+            strcat_s(temp_file_name, SYSTEM_LOG_FILE_NAME_MAX_LEN, p_env_stop);
+            // Store the proper file name
+            strncpy_s(g_log_file_table[SYSTEM_LOG_AR_FILE].file_name, SYSTEM_LOG_FILE_NAME_MAX_LEN, temp_file_name, SYSTEM_LOG_FILE_NAME_MAX_LEN);
+          }
+        }
         g_log_file_table[SYSTEM_LOG_AR_FILE].name_initialized = TRUE;
       }
     }

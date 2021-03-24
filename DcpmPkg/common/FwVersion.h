6ad16fcc8096e1c0f66fc6b70d6ba61890837dd8
Steven Pontsler <steven.pontsler@intel.com>
2021-03-24 23:09:46 +0100

    Updated supported FIS version to 3.2
    Signed-off-by: Steven Pontsler <steven.pontsler@intel.com>

--- DcpmPkg/common/FwVersion.h
+++ DcpmPkg/common/FwVersion.h
@@ -8,8 +8,8 @@
 /** Minimum supported version of FW API: 1.2 **/
 #define MIN_FIS_SUPPORTED_BY_THIS_SW_MAJOR   1
 #define MIN_FIS_SUPPORTED_BY_THIS_SW_MINOR   2
-/** Maximum supported version of FW API: 2.3 **/
+/** Maximum supported version of FW API: 3.2 **/
 #define MAX_FIS_SUPPORTED_BY_THIS_SW_MAJOR    3
-#define MAX_FIS_SUPPORTED_BY_THIS_SW_MINOR    1
+#define MAX_FIS_SUPPORTED_BY_THIS_SW_MINOR    2
 
 #endif /** _FWVERSION_H_ **/

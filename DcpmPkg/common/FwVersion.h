e5212b5f5ab6fa8c3fe29a7db7a047eecad8e578
Steven Pontsler <steven.pontsler@intel.com>
2021-04-09 19:43:31 +0200

    Change supported firmware api version to FIS 3.3
    Signed-off-by: Steven Pontsler <steven.pontsler@intel.com>

--- DcpmPkg/common/FwVersion.h
+++ DcpmPkg/common/FwVersion.h
@@ -8,8 +8,8 @@
 /** Minimum supported version of FW API: 1.2 **/
 #define MIN_FIS_SUPPORTED_BY_THIS_SW_MAJOR   1
 #define MIN_FIS_SUPPORTED_BY_THIS_SW_MINOR   2
-/** Maximum supported version of FW API: 3.2 **/
+/** Maximum supported version of FW API: 3.3 **/
 #define MAX_FIS_SUPPORTED_BY_THIS_SW_MAJOR    3
-#define MAX_FIS_SUPPORTED_BY_THIS_SW_MINOR    2
+#define MAX_FIS_SUPPORTED_BY_THIS_SW_MINOR    3
 
 #endif /** _FWVERSION_H_ **/
